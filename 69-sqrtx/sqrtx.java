e20f01d25cf3604dfac3f6555cb30c401ae970ef
Imdad120 <imdadtaj1@gmail.com>
2026-08-05 20:49:16 +0530

    Time: 1 ms (97.62%) | Memory: 42.8 MB (23.78%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        long lo = 0 , hi = x;
+        while(lo<=hi){
+            long mid =(lo+hi)/2;
+            if(mid*mid==x) return (int)mid;
+            else if( mid*mid > x) hi=mid-1;
+            else lo=mid+1;
+        }
+        return (int)hi;
+        
+    }
+}
