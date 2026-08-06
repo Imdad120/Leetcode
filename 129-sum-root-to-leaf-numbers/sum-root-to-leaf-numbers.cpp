c3c59d6e2403faa7b276b3ab6129576d3040972e
Imdad120 <imdadtaj1@gmail.com>
2026-08-06 14:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (15.61%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,23 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void DFS(TreeNode* root, int cn, int &sum) {
-        if (root == NULL)
-            return;
-
-        cn = cn * 10 + root->val;
-
-        if (root->left == NULL && root->right == NULL) {
-            sum += cn;
-            return;
-        }
-
-        DFS(root->left, cn, sum);
-        DFS(root->right, cn, sum);
-    }
-
     int sumNumbers(TreeNode* root) {
         int sum = 0;
-        DFS(root, 0, sum);
+        stack<pair<TreeNode*,int>>st;
+        st.push(make_pair(root,0));
+        while(!st.empty()){
+            pair<TreeNode*,int> p = st.top();
+            st.pop();
+                   TreeNode*root =p.first;
+                   int cn = p.second;
+
+                   cn = cn*10+root->val;
+                   if(root->left == NULL && root->right == NULL){
+                    sum = sum +cn;
+
+                   }
+
+                   else {
+                    if(root->left != NULL)
+                    st.push(make_pair(root->left,cn));
+                    if(root->right != NULL)
+                    st.push(make_pair(root->right,cn));
+                   }
+        }
         return sum;
+        
     }
 };
