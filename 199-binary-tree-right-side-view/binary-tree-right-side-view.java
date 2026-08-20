75531a0578560b90eeebdc7d24d93e4335834d8b
Imdad Taj <imdadtaj1@gmail.com>
2026-08-20 20:33:56 +0530

    Time: 1 ms (70.07%) | Memory: 43.5 MB (85.19%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode curr = q.poll();
+
+                // Current level ka last node
+                if (i == size - 1) {
+                    ans.add(curr.val);
+                }
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
