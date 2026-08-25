6fee1a48e529bd128587f8034f1cd3bd41459dc4
Imdad Taj <imdadtaj1@gmail.com>
2026-08-25 23:53:04 +0530

    Time: 2145 ms (5.10%) | Memory: 42.2 MB (44.82%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0; 
+        int i =1;
+        while(i < num){
+            if(num % i==0)
+            sum+=i;
+            i++;
+        }
+        if(sum  == num )
+        return true;
+
+        return false;
+    }
+}
