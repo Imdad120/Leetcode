e6c00568607e69f88c9a2fbe96052dbf1418ab5b
Imdad Taj <imdadtaj1@gmail.com>
2026-08-28 13:22:22 +0530

    Time: 6 ms (42.37%) | Memory: 16.1 MB (5.66%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -26,26 +26,43 @@ public:
 
 
         //BETTER APPRAOCH WITH TIME COMPLEXITY OF O(nlogn)
-    int n = nums.size();
-        std::vector<std::pair<int, int>> vec;
-        for (int k = 0; k <=n-1; k++) {
-            vec.push_back({nums[k], k});
+    // int n = nums.size();
+    //     std::vector<std::pair<int, int>> vec;
+    //     for (int k = 0; k <=n-1; k++) {
+    //         vec.push_back({nums[k], k});
+    //     }
+    //     std::sort(vec.begin(), vec.end());
+    //     int i = 0;
+    //     int j = n - 1;
+    //     while (i < j) {
+    //         int sum = vec[i].first + vec[j].first;
+    //         if (sum == target) {
+    //             return {vec[i].second, vec[j].second};
+    //         }
+    //         else if (sum > target) {
+    //             j--;
+    //         }
+    //         else {
+    //             i++;
+    //         }
+    //     }
+    //     return {};
+
+        // OPTIMISED SOLUTION USING HASHMAP 
+        unordered_map<int,int>m;// map to store the first value with their index
+        vector<int>ans;
+        int n=nums.size();
+        for(int i=0; i<=n-1; i++)
+        {
+            int f=nums[i];
+            int s=target-f;
+            if(m.find(s)!=m.end())
+            {
+                ans.push_back(i);
+                ans.push_back(m[s]);
+            }
+            m[f]=i;
         }
-        std::sort(vec.begin(), vec.end());
-        int i = 0;
-        int j = n - 1;
-        while (i < j) {
-            int sum = vec[i].first + vec[j].first;
-            if (sum == target) {
-                return {vec[i].second, vec[j].second};
-            }
-            else if (sum > target) {
-                j--;
-            }
-            else {
-                i++;
-            }
-        }
-        return {};
+        return ans;
     }
 };
