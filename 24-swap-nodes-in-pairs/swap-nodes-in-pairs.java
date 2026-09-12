3e3c1365bb00e0212faa9e3dac015ff46c96481b
Imdad Taj <imdadtaj1@gmail.com>
2026-09-12 14:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (11.95%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -11,25 +11,46 @@
  *     }
  * }
  */
-
+// by three pointer 
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        // Base case
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-        // First two nodes
-        ListNode first = head;
-        ListNode second = head.next;
-
-        // Recursively swap the remaining list
-        first.next = swapPairs(second.next);
-
-        // Swap the first two nodes
-        second.next = first;
-
-        // Return new head
-        return second;
+    public ListNode swapPairs(ListNode head){
+        if(head == null || head.next ==null) return head;
+     ListNode dummy = new ListNode(-1);
+     ListNode a = head;
+     ListNode b = head.next;
+     ListNode c = dummy;
+     while(a!= null && b!= null){
+        c.next = b;
+        a.next =b.next;
+        b.next = a;
+        c=a;
+        a = a.next;
+        if(a !=null)  b= a.next;
+     }
+     return dummy.next;
     }
 }
+
+
+
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         // Base case
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+
+//         // First two nodes
+//         ListNode first = head;
+//         ListNode second = head.next;
+
+//         // Recursively swap the remaining list
+//         first.next = swapPairs(second.next);
+
+//         // Swap the first two nodes
+//         second.next = first;
+
+//         // Return new head
+//         return second;
+//     }
+// }
