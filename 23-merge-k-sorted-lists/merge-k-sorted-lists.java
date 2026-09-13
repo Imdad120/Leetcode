60d2af8718578312b9af21e4c50ba6551df6e6bf
Imdad Taj <imdadtaj1@gmail.com>
2026-09-13 17:49:55 +0530

    Time: 3 ms (80.80%) | Memory: 46.2 MB (97.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,46 +1,67 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0) return null;
-         ArrayList<ListNode> arr = new ArrayList<>();
-       for(ListNode n : lists ) arr.add(n);
-     while(arr.size()>1){
-           ListNode a = arr.get(arr.size()-1);
-           arr.remove(arr.size()-1);
-           ListNode b = arr.get(arr.size()-1);
-           arr.remove(arr.size()-1);
-           ListNode c = merge(a,b);
-           arr.add(c);
-       }
-       return arr.get(0);
+
+        if (lists.length == 0) return null;
+
+        ArrayList<ListNode> arr1 = new ArrayList<>();
+        ArrayList<ListNode> arr2 = new ArrayList<>();
+
+        for (ListNode node : lists) {
+            arr1.add(node);
+        }
+
+        while (arr1.size() > 1) {
+
+            while (arr1.size() > 1) {
+
+                ListNode a = arr1.remove(arr1.size() - 1);
+                ListNode b = arr1.remove(arr1.size() - 1);
+
+                ListNode c = merge(a, b);
+
+                arr2.add(c);
+            }
+
+            // agar ek list bach gayi
+            if (arr1.size() == 1) {
+                arr2.add(arr1.remove(0));
+            }
+
+            // next round
+            arr1.addAll(arr2);
+            arr2.clear();
+        }
+
+        return arr1.get(0);
     }
+
     ListNode merge(ListNode head1, ListNode head2) {
+
         ListNode i = head1;
         ListNode j = head2;
+
         ListNode dummy = new ListNode(-1);
-        ListNode k =dummy;
-        while(i!=null && j!=null){
-            if(i.val <= j.val){
-                k.next=i;
-                i=i.next;
+        ListNode k = dummy;
+
+        while (i != null && j != null) {
+
+            if (i.val <= j.val) {
+                k.next = i;
+                i = i.next;
+            } else {
+                k.next = j;
+                j = j.next;
             }
-            else {
-                k.next=j;
-                j=j.next;
-            }
-            k=k.next;
+
+            k = k.next;
         }
-        if(i==null) k.next = j;
-        else k.next=i;
+
+        if (i != null) {
+            k.next = i;
+        } else {
+            k.next = j;
+        }
+
         return dummy.next;
     }
 }
