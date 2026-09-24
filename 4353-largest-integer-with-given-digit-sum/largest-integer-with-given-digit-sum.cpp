a5e171bf07e7336d59315fae8b365e43c61ca92f
Imdad Taj <imdadtaj1@gmail.com>
2026-09-25 05:14:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (24.52%) - LeetSync

---
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int largestInteger(int n, int s) {
+        if(s>9*n)
+        return -1;
+
+        int ans=0;
+        while(n>0)
+        {
+            int d=min(9,s);
+            ans=ans*10+d;
+            s=s-d;
+            n--;
+
+        }
+        return ans;
+        
+    }
+};
